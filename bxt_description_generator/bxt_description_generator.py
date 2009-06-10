acfb046dfb31568662054907d357acced7a4851e
xiongchiamiov <xiong.chiamiov@gmail.com>
2009-06-09 20:22:39 -0700

    reorganize for egg packaging

---
+++ bxt_description_generator/bxt_description_generator.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This software is licensed under version 2.0 of the WTFPL (see COPYING for details)
+"""
+
+# are we running this standalone, rather than as a module?
+def main():
+	# Check to see if we have all the information we need
+	try:
+		#directory = unicode(sys.argv[1])
+		directory = sys.argv[1]
+		template = sys.argv[2]
+	except IndexError:
+		print "Usage: " + sys.argv[0] + " <directory> <template>"
+		exit()
+	
+	root = Folder(directory)
+	root.scan()
+	
+	env = Environment(loader=PackageLoader('bxt_description_generator', 'templates'))
+	template = env.get_template(template)
+	print template.render(root=root)
+
+if __name__ == '__main__':
+	import sys
+	import os
+	from models import *
+	from jinja2 import Environment, PackageLoader
+	
+	sys.exit(main())
