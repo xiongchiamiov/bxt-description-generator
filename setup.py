4facd723cc8e1c59fb3a8d357b39bc4299db7701
xiongchiamiov <xiong.chiamiov@gmail.com>
2009-06-29 21:10:26 -0700

    changing generated executable's name to something that makes more sense

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def main():
 		license = 'WTFPL',
 		entry_points = """
 			[console_scripts]
-			tgp = bxt_description_generator.bxt_description_generator:main
+			bdg = bxt_description_generator.bxt_description_generator:main
 		""",
 		classifiers = [
 			"Development Status :: 4 - Beta",
